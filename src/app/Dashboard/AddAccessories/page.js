b337bc47ef3d7495059592639a55435fa9589f2b
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2024-02-07 20:49:40 +0600

    Update operation completed

--- src/app/Dashboard/AddAccessories/page.js
+++ src/app/Dashboard/AddAccessories/page.js
@@ -83,7 +83,7 @@ const Page = () => {
            </label>
            <input
              type="text"
-             name="contestName"
+             name="name"
              id="name"
              placeholder="Product Name"
              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
