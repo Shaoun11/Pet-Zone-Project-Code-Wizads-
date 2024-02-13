60464ceb5cd1e36c7632c8c66efb617bcf055dae
afifa <2023afifa@gmail.com>
2024-02-14 00:43:56 +0600

    My cart section and payment history done

--- src/app/Dashboard/Payment/CheckoutForm.js
+++ src/app/Dashboard/Payment/CheckoutForm.js
@@ -1,36 +1,40 @@
 import { AuthContext } from '@/app/Provider/AuthProvider';
-import useAxiosPublic from '@/app/hooks/useAxiosPublic';
+import useCart from '@/app/hooks/useCart';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 
 const CheckoutForm = () => {
-    const [items, setItems] = useState([]);
     const [error, setError] = useState("");
     const [clientSecret, setClientSecret] = useState("");
     const [transactionId, setTransactionId] = useState("");
     const { user } = useContext(AuthContext);
     const stripe = useStripe();
     const elements = useElements();
+    const { mycart, refetch } = useCart();
 
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(
-                    'https://pet-zone-project-next-js.vercel.app/petshop'
-                );
-                const data = await response.json();
-                setItems(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(
+    //                 'http://localhost:5001/mycart'
+    //             );
+    //             const data = await response.json();
+    //             setAllCart(data);
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     };
+    //     fetchData();
+    // }, []);
 
-    // const totalPrice = items.reduce((total, item) => total + item.price, 0);
-    const totalPrice = 999;
+    // const myCart = allCart.filter((cart) => cart.customerEmail === user.email);
+    // console.log(mycart);
+
+
+    const totalPrice = mycart.reduce((total, item) => total + ((parseFloat(item.price)) * (item.quantity)), 0);
     console.log(totalPrice);
 
 
@@ -106,35 +110,34 @@ const CheckoutForm = () => {
         else {
             console.log("Payment intent", paymentIntent);
             if (paymentIntent.status === "succeeded") {
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "Thank you for your payment",
-                    showConfirmButton: false,
-                    timer: 1500
-                });
                 console.log("Transaction id: ", paymentIntent.id);
                 setTransactionId(paymentIntent.id);
 
-                // const payment = {
-                //     email: user.email,
-                //     price: totalPrice,
-                //     transactionId: paymentIntent.id,
-                //     date: new Date(),
-                //     cartIds: items.map(item => item._id),
-                //     menuItemIds: items.map(item => item.menuId),
-                //     status: "pending",
-                // }
-                // console.log("payment saved ", res.data);
-                // if (res.data?.paymentResult?.insertedId) {
-                //     Swal.fire({
-                //         position: "top-end",
-                //         icon: "success",
-                //         title: "Thank you",
-                //         showConfirmButton: false,
-                //         timer: 1500
-                //     });
-                // }
+                // const address = e.target.address.value;
+
+                const payment = {
+                    customerEmail: user.email,
+                    customerName: user.displayName,
+                    // address: address,
+                    amount: totalPrice,
+                    transactionId: paymentIntent.id,
+                    date: new Date(),
+                    cartIds: mycart.map(item => item._id),
+                    petItemIds: mycart.map(item => item.petId),
+                    status: "pending",
+                }
+                const res = await axios.post("http://localhost:5001/payments", payment);
+                console.log("payment saved ", res.data);
+                refetch();
+                if (res.data?.paymentResult?.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Thank you",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
             }
         }
     }
@@ -159,9 +162,23 @@ const CheckoutForm = () => {
                     }}
                 />
                 <div className='text-center'>
-                    <button className="btn bg-red-500 text-white px-10 my-7" type="submit" >
+                    <button className="btn bg-red-500 text-white px-10 my-7" type='submit' disabled={!stripe || !clientSecret}>
                         Pay
                     </button>
+
+                    {/* <button className="btn bg-red-500 text-white px-10 my-7" disabled={!stripe || !clientSecret} onClick={() => document.getElementById('my_modal_1').showModal()}>Pay</button>
+                    <dialog id="my_modal_1" className="modal">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">Please Enter your delivery address</h3>
+                            <div className="modal-action justify-center">
+                                <form method="dialog" className='w-full'>
+                                    <input type='text' name='address' className='w-full rounded-sm border border-[#e0e0e0] bg-white text-base font-medium text-[#6B7280] p-2 mb-3' /><br></br>
+                                    <button type="submit" className="btn bg-red-500 text-white text-lg px-10">Confirm</button>
+                                </form>
+                            </div>
+                        </div>
+                    </dialog> */}
+
                 </div>
                 <p className="text-red-600 text-center">{error}</p>
                 {
